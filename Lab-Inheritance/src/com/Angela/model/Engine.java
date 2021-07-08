aad39c19e95936fc3a2c578174a7d281d100962f
alee-revature <86747521+alee-revature@users.noreply.github.com>
2021-07-08 00:48:51 -0400

    Added day 7 labs

---
+++ Lab-Inheritance/src/com/Angela/model/Engine.java
@@ -0,0 +1,10 @@
+package com.Angela.model;
+
+public class Engine {
+	public void on() {
+		System.out.println("Turning engine on");
+	}
+	public void off() {
+		System.out.println("Turning engine off");
+	}
+}
---
+++ Lab-Inheritance/src/com/Angela/model/Vehicle.java
@@ -0,0 +1,20 @@
+package com.Angela.model;
+
+public class Vehicle {
+	private String name;
+	private long serialNumber;
+	
+	//getters and setters
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	
+	public long getSerialNumber() { return serialNumber; }
+	public void setSerialNumber(long serialNumber) { this.serialNumber = serialNumber; }
+	
+	public void move() {
+		System.out.println("Vehicle is moving...");
+	}
+	public void move(int spaces) {
+		System.out.println("Vehicle moved " + spaces + " spaces");
+	}
+}
